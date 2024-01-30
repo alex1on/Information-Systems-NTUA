5ea321254503f8f7f5dc06a4f133a68427b4410f
Alexandros Ionitsa <alexandros.ionitsa@gmail.com>
2024-01-30 16:58:32 +0200

    Fix NULL Trino column issue and \n on data parse

--- Redis/utils/data_loader.py
+++ Redis/utils/data_loader.py
@@ -1,4 +1,4 @@
-from utils.tables import tables, primary_keys, table_structure
+from utils.tables import table_names, primary_keys, table_structure
 import redis
 import os 
 
@@ -6,19 +6,23 @@ def prepare_redis_hashes(table_name, primary_key, table_columns, lines):
     hashes = {}
 
     for line in lines:
-        values = line.strip('|').split('|')
+        # Seperate column values. (Also clean the \n from the end of each line)
+        values = [value.strip() for value in line.split('|')]
+        #values = line.strip('|').split('|')
 
         # Extract primary key values
         primary_key_values = [values[table_columns.index(pk)] for pk in primary_key]
 
         # Create Redis hash key
-        hash_key = f"{table_name}:{':'.join(primary_key_values)}"
+        hash_key = f"tpcds.{table_name}.{':'.join([f'{pk}:{values[table_columns.index(pk)]}' for pk in primary_key])}"
 
         # Create Redis hash values
-        hash_values = {column: values[table_columns.index(column)] for column in table_columns if column not in primary_key}
+        # For each hash value we choose "columns" that are not in the hash key and its values are not empty. 
+        # We need the last part to properly show NULL values when the redis data is imported to trino.
+        hash_values = {column: values[table_columns.index(column)] for column in table_columns if column not in primary_key or values[table_columns.index(column)] != ''}
 
         # Remove empty values
-        hash_values = {key: value for key, value in hash_values.items() if value != '""'}
+        hash_values = {key: value for key, value in hash_values.items() if value != ''}
 
         # Store hash in dictionary
         hashes[hash_key] = hash_values
@@ -27,15 +31,21 @@ def prepare_redis_hashes(table_name, primary_key, table_columns, lines):
 
 
 def load_data(redis_client):
+    remote_host = 'okeanos-data'
+    file_prefix = "~/Information_systems/tpc_data/"
+    file_suffix = ".dat"
 
-    for i in range(16, len(tables)):
+    for i in range(len(table_names)):
         # Get table info
-        table_name = tables[i]
+        table_name = table_names[i]
         primary_key = primary_keys[i].split(', ')
         table_columns = table_structure[i].split(', ')
 
         # Define data file path
         file_path = "../tpc_data/" + table_name + ".dat"
+        remote_file = file_prefix + table_name + file_suffix
+
+        os.system('scp "%s:%s" "%s"' % (remote_host, remote_file, file_path))
 
         # Read file in chunks
         with open(file_path, 'r') as file:
