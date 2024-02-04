79d347a7813d3c98e086678f68077d6e937dd64d
Alexandros Ionitsa <alexandros.ionitsa@gmail.com>
2024-02-04 02:07:32 +0200

    Update time handling for query runtime

--- queries/query.sh
+++ queries/query.sh
@@ -35,9 +35,23 @@ for db in "${DATABASES[@]}"; do
     duration_query="SELECT date_diff('millisecond', created, \"end\") AS duration FROM system.runtime.queries ORDER BY created DESC OFFSET 1 LIMIT 1;"
     duration=$($TRINO_COMMAND --execute "$duration_query")   
 
+    # Needed as duration is saved as "number" and not directly a string
+    duration_striped="${duration:1:-1}"
+
+    seconds=$((duration_striped / 1000))
+    minutes=$((seconds / 60))
+    hours=$((minutes / 60))
+
+    remaining_milliseconds=$((duration_striped % 1000))
+    remaining_seconds=$((seconds % 60))
+    remaining_minutes=$((minutes % 60))
+
+    # Save to output file with the following format: HH:mm:ss.xxx
+    formatted_duration=$(printf "%02d:%02d:%02d.%03d" "$hours" "$remaining_minutes" "$remaining_seconds" "$remaining_milliseconds")
+
     # Append query and duration to results.txt
     echo "Query: $QUERY" >> results.txt
-    echo "Duration: $duration miliseconds" >> results.txt
+    echo "Duration: $formatted_duration" >> results.txt
     echo "----------------------------" >> results.txt
 
 done
