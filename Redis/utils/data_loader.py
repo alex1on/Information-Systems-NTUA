715f9f1d9bdaf0e6d274804cb541f256801d99b9
Alexandros Ionitsa <alexandros.ionitsa@gmail.com>
2024-02-11 19:05:47 +0200

    Enhance Redis data loader versatility

--- Redis/utils/data_loader.py
+++ Redis/utils/data_loader.py
@@ -29,25 +29,24 @@ def prepare_redis_hashes(table_name, primary_key, table_columns, lines):
 
     return hashes
 
-
-def load_data(redis_client):
+def load_table(redis_client, index, batch_processing, cleanup):
     remote_host = 'okeanos-data'
     file_prefix = "~/Information_systems/tpc_data/"
     file_suffix = ".dat"
+    
+    # Get table info
+    table_name = table_names[index]
+    primary_key = primary_keys[index].split(', ')
+    table_columns = table_structure[index].split(', ')
 
-    for i in range(len(table_names)):
-        # Get table info
-        table_name = table_names[i]
-        primary_key = primary_keys[i].split(', ')
-        table_columns = table_structure[i].split(', ')
+    # Define data file path
+    file_path = "../../tpc_data/" + table_name + ".dat"
+    remote_file = file_prefix + table_name + file_suffix
 
-        # Define data file path
-        file_path = "../tpc_data/" + table_name + ".dat"
-        remote_file = file_prefix + table_name + file_suffix
+    os.system('scp "%s:%s" "%s"' % (remote_host, remote_file, file_path))
 
-        os.system('scp "%s:%s" "%s"' % (remote_host, remote_file, file_path))
-
-        # Read file in chunks
+    # Read file in chunks
+    if batch_processing:
         with open(file_path, 'r') as file:
             chunk_size = 1000  # Adjust the chunk size based on your needs
             while True:
@@ -57,8 +56,33 @@ def load_data(redis_client):
                 # Prepare the Redis hashes for the current chunk
                 hashes = prepare_redis_hashes(table_name, primary_key, table_columns, lines)
 
+
                 # Load hashes in Redis
                 for h in hashes:
                     redis_client.hset(h, mapping=hashes[h])
+    
+    else:
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
 
-        os.system('rm "%s"' % (file_path))
+            # Prepare the Redis hashes for the whole file
+            hashes = prepare_redis_hashes(table_name, primary_key, table_columns, lines)
+
+            # Load hashes in Redis
+            for h in hashes:
+                redis_client.hset(h, mapping=hashes[h])
+
+    if cleanup:
+        clean_file(file_path)
+
+
+def load_data(redis_client, batch_processing=True, table=None, cleanup=True):
+    if table:
+        table_index = table_names.index(table)
+        load_table(redis_client, table_index, batch_processing, cleanup)
+    else:
+        for i in range(len(table_names)):
+            load_table(redis_client, i, batch_processing, cleanup)
+
+def clean_file(file_path):
+    os.system('rm "%s"' % (file_path))
