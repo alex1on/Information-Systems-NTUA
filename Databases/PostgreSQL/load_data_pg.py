f55facd3c38b92342caa643625892c16d4f96e6e
Manos Emmanouilidis <manos.emmanouilidis05@gmail.com>
2024-03-14 17:36:23 +0200

    [PostgreSQL]: Refactor - Use cli arguments and adjust to new configuration format

--- Databases/PostgreSQL/load_data_pg.py
+++ Databases/PostgreSQL/load_data_pg.py
@@ -1,25 +1,41 @@
-# load_data_to_pg.py
 from utils.pg_connection import open_pg_connection, close_pg_connection
 from utils.data_loader import load_table_data
 import json
-import sys
 import os
+import argparse
 
 # Load JSON configuration
 def load_json_config(file_path):
     with open(file_path, 'r') as json_file:
         return json.load(json_file)
 
+# Parse command-line arguments
+def parse_args():
+    parser = argparse.ArgumentParser(description="Load data into the tpcds schema in PostgreSQL.")
+    parser.add_argument('-p', '--partition', type=int, choices=[1, 2],
+                        help="Partition ID to load `pg_tables` from the specified partition.")
+    return parser.parse_args()
+
 if __name__ == "__main__":
 
-    # Determine the table list from command line argument
-    table_list_name = sys.argv[1] if len(sys.argv) > 1 else "all_tables"
+    args = parse_args()
     
     schema_name = "tpcds"
     
     # Read the JSON configuration
     json_config = load_json_config('../distribution_structures.json')
-    tables = json_config.get(table_list_name, [])
+
+    if args.partition:
+        # Read partition-specific tables
+        partition_info = next((p for p in json_config['partitions'] if p['id'] == args.partition), None)
+        if partition_info:
+            tables = partition_info.get('pg_tables', [])
+        else:
+            print(f"Partition with ID {args.partition} not found.")
+            exit(1)
+    else:
+        # Read all_tables
+        tables = json_config.get('all_tables', [])
     
     # Open the database connection
     connection = open_pg_connection()
